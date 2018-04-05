15279507bd08d3f0a5659ed54ce71fb09b2a7383
zyl <zyl@zyl>
2018-04-05 23:43:00 +0800

    测试两个Context

--- venus-server/src/main/java/com/tqmall/zeus/service/init/AfterBeanInit.java
+++ venus-server/src/main/java/com/tqmall/zeus/service/init/AfterBeanInit.java
@@ -4,14 +4,15 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 public class AfterBeanInit implements ApplicationListener<ContextRefreshedEvent> {
-
+	public static int a = 0;
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
+		String name = event.getApplicationContext().getDisplayName();
 		if (event.getApplicationContext().getParent() == null) {// root application context 没有parent，他就是老大.
 			// 需要执行的逻辑代码，当spring容器初始化完成后就会执行该方法。
-			System.out.println("~~~~~~~~~~ok~~~~~");
+			System.out.println("~~~~~~~~~~ok~~~~~" + ++a + name);
 		} else {
-			System.out.println("~~~~~~~~~~ok2~~~~~");
+			System.out.println("~~~~~~~~~~ok2~~~~~" + ++a + name);
 		}
 
 	}
